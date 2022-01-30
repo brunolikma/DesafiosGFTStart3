f554e03c7ba98e3a8fc565a4647781179ba5bde3
brunolikma <brunosssss@gmail.com>
2022-01-30 14:54:25 -0300

    1 a 3

---
+++ src/desafio/gft/start3/java/Xadrez.java
@@ -0,0 +1,19 @@
+package desafio.gft.start3.java;
+
+import java.util.Scanner;
+
+public class Xadrez {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int L = sc.nextInt();
+        int C = sc.nextInt();
+        if (( L + C)  % 2 == 0) {
+            System.out.println("1");
+        }else {
+            System.out.println("0");
+        }
+        sc.close();
+    }
+}
+
